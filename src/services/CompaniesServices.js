ed20d2f2f168322a1f6866c627f673a466ab7d6c
Gabriel Bello <79660211+gabrielbbello@users.noreply.github.com>
2024-03-23 03:41:20 +0000

    feat: finish controller and service to create company

--- src/services/CompaniesServices.js
+++ src/services/CompaniesServices.js
@@ -1,13 +1,26 @@
 const { companies } = require("../models");
 const ErrorHandling = require("../utils/ErrorHandling");
+const { hash } = require("bcrypt");
 
 class CompaniesServices {
   async createCompany(name, sector, email, password, cnpj) {
     if (!name || !sector || !email || !password || !cnpj) {
-      throw new ErrorHandling("Todos os campos são obrigatórios.");
+      throw new ErrorHandling("All fields are required");
     }
 
-    const company = await companies.create({ name, sector, email, password, cnpj });
+    const existingCompany = await companies.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (existingCompany) {
+      throw new ErrorHandling("This company already exists");
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const company = await companies.create({ name, sector, email, password: hashedPassword, cnpj });
 
     return company;
   }
